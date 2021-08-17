2670308098feea19c3f5c27f17c008cea8f5903f
hotShot1211 <anshusuperstar15@gmail.com>
2021-08-17 12:51:00 +0530

    adding symbols in div

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,60 @@
 //One of something use module
 //multiples of something factories
 
-const gameBoard = () => {
-
+//factory for player
+const player = (name, symbol) => {
+    return {name, symbol};
 }
+
+//module to controller display
+const displayController = () => {
+    let board = gameBoard;
+    let resultDiv = document.querySelector(".result");
+    const symbolDisplay = (symbol, div) => {
+        div.innerText = symbol;
+        let num = div.getAttribute("data-value");
+        board.gameboard[num] = symbol;
+        console.log(board.gameboard);
+    }
+
+    const winnerDisplay = (winner) => {
+        resultDiv.innerText = `${winner} wins!!!`;
+    }
+
+    const tieDisplay = () => {
+        resultDiv.innerText = `It's a tie!`;
+    }
+
+    return {symbolDisplay, winnerDisplay, tieDisplay};
+};
+
+//gameboard
+const gameBoard = (() => {
+    let gameboard = [];
+
+    return {gameboard};
+})();
+
+//game 
+const game = (() => {
+    let playerOne = player("me", "X");
+    let playerTwo = player("You", "O");
+    let currPlayer = playerOne;
+    let display = displayController();
+    let divs = document.querySelectorAll(".game-board div");
+    divs.forEach(div => {
+        div.addEventListener("click", () => {
+            display.symbolDisplay(currPlayer.symbol, div);
+            if(currPlayer == playerOne){
+                currPlayer = playerTwo;
+            }
+            else {
+                currPlayer = playerOne;
+            }
+        })
+    })
+
+})();
+
+let gamer = game;
+game;
